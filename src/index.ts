e3c59e43ea8dfb4fd083a6279738ec0470ff0327
LeopoldHsing <LeopoldHsing@gmail.com>
2024-07-05 20:38:46 -0500

    feat: setup database connection in the entry file

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
 import express, {Request, Response} from "express";
 import cors from "cors";
+import * as mongoose from "mongoose";
+
+// 1. get env
+require('dotenv').config();
+
+// 2. connect to database
+let connectionString = process.env.MONGODB_CONNECT_STRING;
+if (!connectionString) {
+  connectionString = `mongodb://${process.env.MONGODB_ROOT_USERNAME}:${process.env.MONGODB_ROOT_PASSWORD}@${process.env.MONGODB_HOST}:${process.env.MONGODB_PORT}/`
+}
+mongoose.connect(connectionString as string).then(() => console.log("Connected to database!"))
 
 const app = express();
 app.use(express.json());
