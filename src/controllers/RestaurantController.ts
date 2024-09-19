0d7ce542010b3278b378c6ca025d747e1274293c
LeopoldHsing <LeopoldHsing@gmail.com>
2024-09-19 18:29:18 -0500

    feat: feat: add search functionality

--- src/controllers/RestaurantController.ts
+++ src/controllers/RestaurantController.ts
@@ -4,7 +4,7 @@ import { Restaurant } from "../models/restaurant";
 const searchRestaurant = async (req: Request, res: Response) => {
   try {
     const city = req.params.city;
-    const searchQuery = (req.query.searchQuery as string) || '';
+    const keyword = (req.query.keyword as string) || '';
     const selectedCuisines = (req.query.selectedCuisines as string) || '';
     const sortOption = (req.query.sortOption as string) || 'lastUpdated';
     const page = parseInt(req.query.page as string) || 1;
@@ -15,7 +15,7 @@ const searchRestaurant = async (req: Request, res: Response) => {
     query['city'] = new RegExp(city, 'i');
     const cityCheck = await Restaurant.countDocuments(query);
     if (!cityCheck) {
-      return res.status(422).json({
+      return res.status(200).json({
         data: [],
         pagination: {
           total: 0,
@@ -32,8 +32,8 @@ const searchRestaurant = async (req: Request, res: Response) => {
     }
 
     // add conditions into the query object
-    if (searchQuery) {
-      const searchRegex = new RegExp(searchQuery, "i");
+    if (keyword) {
+      const searchRegex = new RegExp(keyword, "i");
       query["$or"] = [
         { restaurantName: searchRegex },
         { cuisines: { $in: [searchRegex] } },
